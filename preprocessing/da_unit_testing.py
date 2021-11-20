0467eed432486e536d5be3d27599c00b1c387a08
emedema <medema.emily@gmail.com>
2021-11-19 16:14:25 -0800

    fixing merge conflicts

--- preprocessing/da_unit_testing.py
+++ preprocessing/da_unit_testing.py
@@ -1,6 +1,19 @@
 import unittest
 
-from data_augmentation import DataAug
+from data_augmentation import (
+    get_files,
+    read_image,
+    show_image,
+    fill,
+    horizontal_shift,
+    vertical_shift,
+    brightness,
+    zoom,
+    channel_shift,
+    horizontal_flip,
+    vertical_flip,
+    rotation,
+)
 
 
 class TestDA(unittest.TestCase):
@@ -8,78 +21,73 @@ class TestDA(unittest.TestCase):
         """
         Tests if the file names are gotten
         """
-        da = DataAug("../test_images_kaggle/images")
-        files = da.get_files()
-        self.assertEqual(len(files), 50)
+        self.assertEqual()
 
     def test_read_image(self):
         """
         Tests if the image files are read
         """
-        da = DataAug("../test_images_kaggle/images")
-        files = da.get_files()
-        img = da.read_image(files[0])
-        self.assertNotEqual(img.size, 0)
+        self.assertEqual()
 
-    # def test_show_image(self):
-    #     """
-    #     Tests if the images are shown
-    #     """
-    #     self.assertEqual()
+    def test_show_image(self):
+        """
+        Tests if the images are shown
+        """
+        self.assertEqual()
 
-    # def test_fill(self):
-    #     """
-    #     Tests if the images are filled
-    #     """
-    #     self.assertEqual()
+    def test_fill(self):
+        """
+        Tests if the images are filled
+        """
+        self.assertEqual()
 
-    # def test_horizontal_shift(self):
-    #     """
-    #     Tests if the images are shifted horizontally
-    #     """
-    #     self.assertEqual()
+    def test_horizontal_shift(self):
+        """
+        Tests if the images are shifted horizontally
+        """
+        self.assertEqual()
 
-    # def test_vertical_shift(self):
-    #     """
-    #     Tests if the images are shifted vertically
-    #     """
-    #     self.assertEqual()
+    def test_vertical_shift(self):
+        """
+        Tests if the images are shifted vertically
+        """
+        self.assertEqual()
 
-    # def test_brightness(self):
-    #     """
-    #     Tests if the images are brightened
-    #     """
-    #     self.assertEqual()
+    def test_brightness(self):
+        """
+        Tests if the images are brightened
+        """
+        self.assertEqual()
 
-    # def test_zoom(self):
-    #     """
-    #     Tests if the images are zoomed
-    #     """
-    #     self.assertEqual()
+    def test_zoom(self):
+        """
+        Tests if the images are zoomed
+        """
+        self.assertEqual()
 
-    # def test_channel_shift(self):
-    #     """
-    #     Tests if the channel is shifted
-    #     """
-    #     self.assertEqual()
+    def test_channel_shift(self):
+        """
+        Tests if the channel is shifted
+        """
+        self.assertEqual()
 
-    # def test_horizontal_flip(self):
-    #     """
-    #     Tests if the image is flipped horizontally
-    #     """
-    #     self.assertEqual()
+    def test_horizontal_flip(self):
+        """
+        Tests if the image is flipped horizontally
+        """
+        self.assertEqual()
 
-    # def test_vertical_flip(self):
-    #     """
-    #     Tests if the image is flipped vertically
-    #     """
-    #     self.assertEqual()
+    def test_vertical_flip(self):
+        """
+        Tests if the image is flipped vertically
+        """
+        self.assertEqual()
 
-    # def test_rotation(self):
-    #     """
-    #     Tests if the image is rotated
-    #     """
-    #     self.assertEqual()
+    def test_rotation(self):
+        """
+        Tests if the image is rotated
+        """
+        self.assertEqual()
 
 
 if __name__ == "__main__":
