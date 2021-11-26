983827d70f3f594183814aa26daf2d4489d58691
Niklas Tecklenburg <2465258T@student.gla.ac.uk>
2021-11-25 18:35:56 -0800

    Add triplet_loss.py

---
+++ src/training/triplet_loss_function.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+
+def triplet_loss(y_pred, alpha = 0.2):
+    """
+    Implementation of the triplet loss as defined by formula (3)
+    
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+              (Call with None)  
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor images, of shape (None, 128)
+            positive -- the encodings for the positive images, of shape (None, 128)
+            negative -- the encodings for the negative images, of shape (None, 128)
+    
+    Returns:
+    loss -- real number, value of the loss
+    """
+    
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+    
+    # Compute the (encoding) distance between the anchor and the positive
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,positive)),axis=-1)
+    # Compute the (encoding) distance between the anchor and the negative
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,negative)),axis=-1)
+    # Subtract the two previous distances and add alpha.
+    basic_loss = tf.add(tf.subtract(pos_dist,neg_dist), alpha)
+    # Take the maximum of basic_loss and 0.0. Sum over the training examples.
+    loss = tf.reduce_sum(tf.maximum(basic_loss,0))
+    
+    return loss
