d61eb42464e99fe0b2a4a4e6022b3de57a2d781e
Niklas Tecklenburg <2465258T@student.gla.ac.uk>
2021-11-26 17:47:43 -0800

    Minor Adjustments at CNN

--- docs/exploration/CNN_on_Remote/MainTrain.py
+++ docs/exploration/CNN_on_Remote/MainTrain.py
@@ -19,14 +19,14 @@ batch_size = 200
 # Adjust
 path = ''
 img_size = 128
-img_rcc = np.load(path + 'img_rcc_' + str(img_size))
-img_lcc = np.load(path + 'img_lcc_' + str(img_size))
-img_rmlo = np.load(path + 'img_rmlo_' + str(img_size))
-img_lmlo = np.load(path + 'img_lmlo_' + str(img_size))
-lab_rcc = np.load(path + 'lab_rcc_' + str(img_size))
-lab_lcc = np.load(path + 'lab_lcc_' + str(img_size))
-lab_rmlo = np.load(path + 'lab_rmlo_' + str(img_size))
-lab_lmlo = np.load(path + 'lab_lmlo_' + str(img_size))
+img_rcc = np.load(path + 'img_rcc_' + str(img_size)+'.npy')
+img_lcc = np.load(path + 'img_lcc_' + str(img_size)+'.npy')
+img_rmlo = np.load(path + 'img_rmlo_' + str(img_size)+'.npy')
+img_lmlo = np.load(path + 'img_lmlo_' + str(img_size)+'.npy')
+lab_rcc = np.load(path + 'lab_rcc_' + str(img_size)+'.npy')
+lab_lcc = np.load(path + 'lab_lcc_' + str(img_size)+'.npy')
+lab_rmlo = np.load(path + 'lab_rmlo_' + str(img_size)+'.npy')
+lab_lmlo = np.load(path + 'lab_lmlo_' + str(img_size)+'.npy')
 
 rcc_data_train, rcc_data_test, rcc_labels_train, rcc_labels_test = train_test_split(img_rcc, lab_rcc, test_size=0.2, random_state=42)
 lcc_data_train, lcc_data_test, lcc_labels_train, lcc_labels_test = train_test_split(img_lcc, lab_lcc, test_size=0.2, random_state=42)
@@ -42,9 +42,8 @@ network_train = build_model(input_shape,network)
 optimizer = Adam(lr = 0.00006)
 network_train.compile(loss=None,optimizer=optimizer)
 network_train.summary()
-plot_model(network_train,show_shapes=True, show_layer_names=True, to_file='02 model.png')
 print(network_train.metrics_names)
-network_train.load_weights('mnist-160k_weights.h5')
+#network_train.load_weights('mnist-160k_weights.h5')
 
 
 t_start = time.time()
