b31ae5703f55a4db11fbc1afc1606531ca4d7c7c
Niklas Tecklenburg <2465258T@student.gla.ac.uk>
2021-11-29 16:20:22 -0800

    Add Saving every n-iterations and loading model option

--- docs/exploration/CNN_on_Remote/MainTrain.py
+++ docs/exploration/CNN_on_Remote/MainTrain.py
@@ -9,12 +9,12 @@ from keras.utils import plot_model
 from CNNTripletModel import build_network, build_model
 from BatchBuilder import get_batch_random
 
-
+train_from_scratch = True
 
 img_size = 128
 input_shape = (img_size,img_size,1)
 
-evaluate_every = 10
+evaluate_save_every = 10
 n_val = 5
 batch_size = 50
 
@@ -38,13 +38,16 @@ data_train = [rcc_data_train, lcc_data_train, rmlo_data_train, lmlo_data_train]
 labels_train = [rcc_labels_train, lcc_labels_train, rmlo_labels_train, lmlo_labels_train]
 
 
-network = build_network(input_shape,embeddingsize=100)
+if Train from scratch:
+    network = build_network(input_shape,embeddingsize=100)
+else:
+    network = load_model('Saving_Test')
+    
 network_train = build_model(input_shape,network)
 optimizer = Adam(lr = 0.00006)
 network_train.compile(loss=None,optimizer=optimizer)
 network_train.summary()
 print(network_train.metrics_names)
-#network_train.load_weights('mnist-160k_weights.h5')
 
 
 t_start = time.time()
@@ -54,8 +57,8 @@ for i in range(3):
     triplets = get_batch_random(data_train, labels_train, batch_size)
     loss = network_train.train_on_batch(triplets, None)
     print(loss)
-    # n_iteration += 1
-    # if i % evaluate_every == 0:
-    #     print("\n ------------- \n")
-    #     print("[{3}] Time for {0} iterations: {1:.1f} mins, Train Loss: {2}".format(i, (time.time()-t_start)/60.0,loss,n_iteration))
-    #     probs,yprob = compute_probs(network,test_images[:n_val,:,:,:],y_test_origin[:n_val])
+    n_iteration += 1
+    if i % evaluate_save_every == 0:
+        print("\n ------------- \n")
+        network.save('Network')
+        # evaluate on test data (maybe avg difference positive and anchor and negative and anchor)
