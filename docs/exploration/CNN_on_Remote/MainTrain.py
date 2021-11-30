6bbf7f89c60124140599f12c7f07019524df79e4
Niklas Tecklenburg <2465258T@student.gla.ac.uk>
2021-11-29 18:20:48 -0800

    Adjust errors in Code

--- docs/exploration/CNN_on_Remote/MainTrain.py
+++ docs/exploration/CNN_on_Remote/MainTrain.py
@@ -5,9 +5,11 @@ from sklearn.model_selection import train_test_split
 
 from keras.optimizers import Adam
 from keras.utils import plot_model
+import keras
 
 from CNNTripletModel import build_network, build_model
 from BatchBuilder import get_batch_random
+from Evaluate import calculate_avg_dist
 
 train_from_scratch = True
 
@@ -36,12 +38,14 @@ lmlo_data_train, lmlo_data_test, lmlo_labels_train, lmlo_labels_test = train_tes
 
 data_train = [rcc_data_train, lcc_data_train, rmlo_data_train, lmlo_data_train]
 labels_train = [rcc_labels_train, lcc_labels_train, rmlo_labels_train, lmlo_labels_train]
+data_test = [rcc_data_test, lcc_data_test, rmlo_data_test, lmlo_data_test]
+labels_test = [rcc_labels_test, lcc_labels_test, rmlo_labels_test, lmlo_labels_test]
 
 
-if Train from scratch:
+if train_from_scratch:
     network = build_network(input_shape,embeddingsize=100)
 else:
-    network = load_model('Saving_Test')
+    network = keras.load_model('Network')
     
 network_train = build_model(input_shape,network)
 optimizer = Adam(lr = 0.00006)
@@ -56,11 +60,12 @@ for i in range(3):
     triplets = get_batch_random(data_train, labels_train, batch_size)
     loss = network_train.train_on_batch(triplets, None)
     print(loss)
-    if i % evaluate_every == 0:
-            time_passed = (time.time()-t_start)/60.0
-            test_dat = get_batch_random(data_test, labels_test, test_batch_size)
-            test_dist_p = calculate_avg_dist(network, test_dat[0], test_dat[1])
-            test_dist_n = calculate_avg_dist(network, test_dat[0], test_dat[2])
-            print("\n ------------- \n")
-            print("Time for {0} iterations: {1:.1f} mins, Train Loss: {2}, Avg. Distance P: {3:.4f}, Avg. Distance N: {4:.4f}".format(i, time_passed, loss, test_dist_p, test_dist_n))
+    if i % evaluate_save_every == 0:
+        network.save('Network')
+        time_passed = (time.time()-t_start)/60.0
+        test_dat = get_batch_random(data_test, labels_test, test_batch_size)
+        test_dist_p = calculate_avg_dist(network, test_dat[0], test_dat[1])
+        test_dist_n = calculate_avg_dist(network, test_dat[0], test_dat[2])
+        print("\n ------------- \n")
+        print("Time for {0} iterations: {1:.1f} mins, Train Loss: {2}, Avg. Distance P: {3:.4f}, Avg. Distance N: {4:.4f}".format(i, time_passed, loss, test_dist_p, test_dist_n))
     
