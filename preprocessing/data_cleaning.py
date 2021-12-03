3059a40ad69ef091459bb769a34cc3b9de1482dd
marieke6 <65109337+marieke6@users.noreply.github.com>
2021-12-03 14:26:29 -0800

    more tests implemented for cleaning

--- preprocessing/data_cleaning.py
+++ preprocessing/data_cleaning.py
@@ -19,25 +19,28 @@ print(df.dtypes)
 # Remove duplicates
 # Handle missing data as a %
 # select numeric and non numeric columns
-df_non_numeric = df.select_dtypes(exclude=[np.number])
-non_numeric_cols = df_non_numeric.columns.values
+def split_data():
+    df_non_numeric = df.select_dtypes(exclude=[np.number])
+    non_numeric_cols = df_non_numeric.columns.values
 
-df_numeric = df.select_dtypes(include=[np.number])
-numeric_cols = df_numeric.columns.values
+    df_numeric = df.select_dtypes(include=[np.number])
+    numeric_cols = df_numeric.columns.values
 
 
 
 #values missing in each column
-col_list = list()
-value_list = list()
+def missing_data():
+    col_list = list()
+    value_list = list()
 
-for col in df.columns:
-    pc_missing = np.mean(df[col].isnull())*100
-    col_list.append(col)
-    value_list.append(pc_missing)
-pc_missing_df = pd.DataFrame()
-pc_missing_df['col'] = col_list
-pc_missing_df['pc_missing'] = value_list
+    for col in df.columns:
+
+        pc_missing = np.mean(df[col].isnull())*100
+        col_list.append(col)
+        value_list.append(pc_missing)
+        pc_missing_df = pd.DataFrame()
+        pc_missing_df['col'] = col_list
+        pc_missing_df['pc_missing'] = value_list
 
 # input missing values?
 
