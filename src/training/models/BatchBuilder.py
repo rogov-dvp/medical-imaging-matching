073c3f797da1f6e5d324f8bfd93179ed8215046b
Niklas Tecklenburg <2465258T@student.gla.ac.uk>
2022-01-28 18:56:35 -0800

    move model to model folder

--- src/training/models/BatchBuilder.py
+++ src/training/models/BatchBuilder.py
@@ -24,9 +24,7 @@ def get_batch_random_demo(data_train, data_labels, batch_size):
         triplets[0][i,:,:,:] = data_train[pos_ind[i],0,:,:,:]
         triplets[1][i,:,:,:] = data_train[pos_ind[i],1,:,:,:]
         
-        # WE HAVE TO CHECK HERE THAT WE TAKE SAME TYPE???
-        
-        #Pick negative image of different patient different from different patient
+        #Pick negative image of different patient 
         while data_labels[neg_ind[i]] == data_labels[pos_ind[i]]:
             neg_ind[i] -= 1
         triplets[2][i,:,:,:] = data_train[neg_ind[i],0,:,:,:]
@@ -37,6 +35,10 @@ def get_batch_random(data_train, data_labels, batch_size):
     Create batch of APN triplets with a complete random strategy
     """ 
     h, w, c = data_train[0][0,0].shape
+    
+    # Inserted
+    c = 3
+    
     triplets=[np.zeros((batch_size, h, w, c)) for i in range(3)]
     
     cat = random.randint(0,len(data_train)-1)
@@ -45,63 +47,83 @@ def get_batch_random(data_train, data_labels, batch_size):
         neg_ind = random.randint(0, len(data_train[cat])-1)
         
         #Pick one random anchor and positive image
-        triplets[0][i,:,:,:] = data_train[cat][pos_ind,0,:,:,:]
-        triplets[1][i,:,:,:] = data_train[cat][pos_ind,1,:,:,:]
+        triplets[0][i,:,:,0] = data_train[cat][pos_ind,0,:,:,0]
+        triplets[1][i,:,:,0] = data_train[cat][pos_ind,1,:,:,0]
+        triplets[0][i,:,:,1] = data_train[cat][pos_ind,0,:,:,0]
+        triplets[1][i,:,:,1] = data_train[cat][pos_ind,1,:,:,0]
+        triplets[0][i,:,:,2] = data_train[cat][pos_ind,0,:,:,0]
+        triplets[1][i,:,:,2] = data_train[cat][pos_ind,1,:,:,0]
         
         #Pick negative image of different patient different from different patient
-        counter = 0
         while data_labels[cat][neg_ind] == data_labels[cat][pos_ind]:
             neg_ind = random.randint(0, len(data_train[cat])-1)
-            counter += 1
-            if counter > 100:
-                return "Could not find negative in 30 tries please check the data and rerun"
-        triplets[2][i,:,:,:] = data_train[cat][neg_ind,0,:,:,:]
+        triplets[2][i,:,:,0] = data_train[cat][neg_ind,0,:,:,0]
+        triplets[2][i,:,:,1] = data_train[cat][neg_ind,0,:,:,0]
+        triplets[2][i,:,:,2] = data_train[cat][neg_ind,0,:,:,0]
         cat += 1
         cat = cat % len(data_train)
+        
     return triplets
 
 # def get_batch_hard(draw_batch_size,hard_batchs_size,norm_batchs_size,network,s="train"):
 #     """
-#     Create batch of APN "hard" triplets
+# Create batch of APN "hard" triplets
     
-#     Arguments:
-#     draw_batch_size -- integer : number of initial randomly taken samples   
-#     hard_batchs_size -- interger : select the number of hardest samples to keep
-#     norm_batchs_size -- interger : number of random samples to add
+    Arguments:
+     draw_batch_size -- integer : number of initial randomly taken samples   
+     hard_batchs_size -- interger : select the number of hardest samples to keep
+     norm_batchs_size -- interger : number of random samples to add
 
 #     Returns:
 #     triplets -- list containing 3 tensors A,P,N of shape (hard_batchs_size+norm_batchs_size,w,h,c)
 #     """
-#     if s == 'train':
-#         X = dataset_train
-#     else:
-#         X = dataset_test
+     if s == 'train':
+         X = dataset_train
+     else:
+        X = dataset_test
 
-#     m, w, h,c = X[0].shape
+    m, w, h,c = X[0].shape
     
     
 #     #Step 1 : pick a random batch to study
-#     studybatch = get_batch_random(draw_batch_size,s)
+     studybatch = get_batch_random(draw_batch_size,s)
     
 #     #Step 2 : compute the loss with current network : d(A,P)-d(A,N). The alpha parameter here is omited here since we want only to order them
-#     studybatchloss = np.zeros((draw_batch_size))
+     studybatchloss = np.zeros((draw_batch_size))
     
 #     #Compute embeddings for anchors, positive and negatives
-#     A = network.predict(studybatch[0])
-#     P = network.predict(studybatch[1])
-#     N = network.predict(studybatch[2])
+     A = network.predict(studybatch[0])
+     P = network.predict(studybatch[1])
+     N = network.predict(studybatch[2])
     
 #     #Compute d(A,P)-d(A,N)
-#     studybatchloss = np.sum(np.square(A-P),axis=1) - np.sum(np.square(A-N),axis=1)
+     studybatchloss = np.sum(np.square(A-P),axis=1) - np.sum(np.square(A-N),axis=1)
     
-#     #Sort by distance (high distance first) and take the 
-#     selection = np.argsort(studybatchloss)[::-1][:hard_batchs_size]
+#     Sort by distance (high distance first) and take the 
+     selection = np.argsort(studybatchloss)[::-1][:hard_batchs_size]
     
 #     #Draw other random samples from the batch
-#     selection2 = np.random.choice(np.delete(np.arange(draw_batch_size),selection),norm_batchs_size,replace=False)
+     selection2 = np.random.choice(np.delete(np.arange(draw_batch_size),selection),norm_batchs_size,replace=False)
     
-#     selection = np.append(selection,selection2)
+     selection = np.append(selection,selection2)
     
-#     triplets = [studybatch[0][selection,:,:,:], studybatch[1][selection,:,:,:], studybatch[2][selection,:,:,:]]
+     triplets = [studybatch[0][selection,:,:,:], studybatch[1][selection,:,:,:], studybatch[2][selection,:,:,:]]
     
-#     return triplets
+     return triplets
+#     def build_network(input_shape, embeddingsize):
+#    '''
+#    Define the neural network to learn image similarity
+#    Input : 
+#            input_shape : shape of input images
+#            embeddingsize : vectorsize used to encode our picture   
+#    '''
+#     # Convolutional Neural Network
+
+
+
+
+
+
+#     return network
+
+# Loss Function
---
+++ src/training/models/MainRead.py
@@ -0,0 +1,21 @@
+import numpy as np
+from Read_images import read_img_to_array
+
+#Adjust
+#root_dir = '/home/capstone/Desktop/CADAnonymized'
+root_dir = './Data'
+store_dir = ''
+
+img_size = 28
+img_rcc,img_lcc,img_rmlo,img_lmlo,lab_rcc,lab_lcc,lab_rmlo,lab_lmlo = read_img_to_array(root_dir, img_size)
+
+np.save(store_dir + 'img_rcc_' + str(img_size),img_rcc)
+np.save(store_dir + 'img_lcc_' + str(img_size),img_lcc)
+np.save(store_dir + 'img_rmlo_' + str(img_size),img_rmlo)
+np.save(store_dir + 'img_lmlo_' + str(img_size),img_lmlo)
+
+np.save(store_dir + 'lab_rcc_' + str(img_size),lab_rcc)
+np.save(store_dir + 'lab_lcc_' + str(img_size),lab_lcc)
+np.save(store_dir + 'lab_rmlo_' + str(img_size),lab_rmlo)
+np.save(store_dir + 'lab_lmlo_' + str(img_size),lab_lmlo)
+
